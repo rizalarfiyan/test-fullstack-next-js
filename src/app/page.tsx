74eff18496bc16470a06da20b2d3ef328d31c8c0
Muhamad Rizal Arfiyan <rizal.arfiyan.23@gmail.com>
2024-01-30 15:23:04 +0700

    feat: update metadata
    Signed-off-by: Muhamad Rizal Arfiyan <rizal.arfiyan.23@gmail.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className='w-full h-full min-h-screen flex-col flex justify-center gap-6 items-center'>
       <div className='text-center'>
-        <h1 className='font-semibold text-slate-800 text-2xl lg:text-4xl'>Test Fullstack Next.js Test</h1>
+        <h1 className='font-semibold text-slate-800 text-2xl lg:text-4xl'>Test Fullstack Next.js</h1>
         <p className='text-slate-600'>
           by{' '}
           <a className='underline decoration-blue-400 underline-offset-2' href='https://github.com/rizalarfiyan'>
